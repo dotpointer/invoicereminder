ab6b566b2b927676b84a47b1aff17bc16c547638
Robert Klebe <dotpointer@gmail.com>
2017-02-17 00:55:56 +0100

    Rewriting loops and refining code.

--- include/functions.php
+++ include/functions.php
@@ -1,14 +1,21 @@
 <?php
 	session_start();
 	# changelog
-	# 2017-02-14 17:57:26
+	# 2017-02-14 17:57:26 - initial version
+	# 2017-02-17 00:54:02 - updating
 
 	/*
 	CREATE TABLE invoicenagger_debtors(
 		id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT NOT NULL,
 		invoicenumber INT NOT NULL,
+		name TINYTEXT NOT NULL,
+		address TINYTEXT NOT NULL,
+		zipcode TINYTEXT NOT NULL,
+		city TINYTEXT NOT NULL,
+		orgno TINYTEXT NOT NULL,
 		amount FLOAT NOT NULL,
 		collectioncost FLOAT NOT NULL DEFAULT 0,
+		debtor TEXT NOT NULL,
 		remindercost FLOAT NOT NULL DEFAULT 0,
 		percentage FLOAT NOT NULL,
 		email TINYTEXT NOT NULL,
@@ -19,6 +26,7 @@
 		mails_sent INT NOT NULL,
 		last_reminder DATETIME NOT NULL,
 		reminder_days INT NOT NULL DEFAULT 30,
+		day_of_month int not null default 0,
 		template TINYTEXT NOT NULL,
 		created DATETIME NOT NULL,
 		updated DATETIME NOT NULL
@@ -36,12 +44,25 @@
 	define('SITE_SHORTNAME', 'invoicenagger');
 	define('DATABASE_NAME', 'invoicenagger');
 
+	define('DEBTOR_STATUS_ACTIVE', 1);
+	define('DEBTOR_STATUS_ERROR', -1);
+	define('DEBTOR_STATUS_INACTIVE', 0);
+
 	define('LOG_TYPE_ERROR', -1);
 	define('LOG_TYPE_MAIL_SENT', 1);
 
-	define('DEBTOR_STATUS_ERROR', -1);
-	define('DEBTOR_STATUS_INACTIVE', 0);
-	define('DEBTOR_STATUS_ACTIVE', 1);
+	define('REPLY_TO', 'Your Name <your@email.com>');
+	define('FROM', 'Your Name <your@email.com>');
+
+	define('TEMPLATE_DEFAULT', 'default');
+	define('TEMPLATE_DIR', 'templates/');
+
+	# verbosity
+	define('VERBOSE_OFF', 0);		# no info at all
+	define('VERBOSE_ERROR', 1);		# only errors
+	define('VERBOSE_INFO', 2);		# above and things that changes
+	define('VERBOSE_DEBUG', 3);		# above and verbose info
+	define('VERBOSE_DEBUG_DEEP', 4);
 
 	require_once('config.php');
 	require_once('base3.php');
@@ -60,6 +81,133 @@
 	# register a shutdown function
 	register_shutdown_function('shutdown_function', $link);
 
-	define('TEMPLATE_DIR', 'templates/');
-	define('TEMPLATE_DEFAULT', 'default');
+	/**
+	* array_merge_recursive does indeed merge arrays, but it converts values with duplicate
+	* keys to arrays rather than overwriting the value in the first array with the duplicate
+	* value in the second array, as array_merge does. I.e., with array_merge_recursive,
+	* this happens (documented behavior):
+	*
+	* array_merge_recursive(array('key' => 'org value'), array('key' => 'new value'));
+	* => array('key' => array('org value', 'new value'));
+	*
+	* array_merge_recursive_distinct does not change the datatypes of the values in the arrays.
+	* Matching keys' values in the second array overwrite those in the first array, as is the
+	* case with array_merge, i.e.:
+	*
+	* array_merge_recursive_distinct(array('key' => 'org value'), array('key' => 'new value'));
+	* => array('key' => 'new value');
+	*
+	* Parameters are passed by reference, though only for performance reasons. They're not
+	* altered by this function.
+	*
+	* @param array $array1
+	* @param mixed $array2
+	* @author daniel@danielsmedegaardbuus.dk
+	* @return array
+	*/
+	function &array_merge_recursive_distinct(array &$array1, &$array2 = null) {
+		$merged = $array1;
+
+		if (is_array($array2)) {
+			foreach ($array2 as $key => $val) {
+				if (is_array($array2[$key])) {
+					$merged[$key] = isset($merged[$key]) && is_array($merged[$key]) ? array_merge_recursive_distinct($merged[$key], $array2[$key]) : $array2[$key];
+				} else {
+					$merged[$key] = $val;
+				}
+			}
+		}
+		return $merged;
+	}
+
+	# to disable all debtors with a certain template
+	function disable_debtors_with_template($link, $template) {
+		# disable this debtor
+		$sql = '
+			UPDATE
+				invoicenagger_debtors
+			SET
+				status="'.dbres($link,DEBTOR_STATUS_ERROR).'",
+				updated="'.dbres($link, date('Y-m-d H:i:s')).'"
+			WHERE
+				template="'.dbres($link, $template).'"
+				';
+		$r = db_query($link, $sql);
+		if ($r === false) {
+			echo db_error($link);
+			die(1);
+		}
+		return true;
+	}
+
+
+	# to disable a debtor
+	function set_debtor_status($link, $id, $status) {
+		# disable this debtor
+		$sql = '
+			UPDATE
+				invoicenagger_debtors
+			SET
+					status="'.dbres($link, $status).'",
+					updated="'.dbres($link, date('Y-m-d H:i:s')).'"
+			WHERE id="'.dbres($link, $id).'"';
+		$r = db_query($link, $sql);
+		if ($r === false) {
+			echo db_error($link);
+			die(1);
+		}
+	}
+
+	# debug printing
+	function cl($link, $level, $s) {
+
+		global $config;
+
+
+		# find out level of verbosity
+		switch ($level) {
+			default:
+			case VERBOSE_ERROR:
+				$l = 'E';
+				break;
+			case VERBOSE_INFO:
+				$l = 'I';
+				break;
+			case VERBOSE_DEBUG:
+			case VERBOSE_DEBUG_DEEP:
+				$l = 'D';
+				break;
+
+		}
+
+		# is verbosity on and level is enough?
+		if ($config['main']['verbose'] && $config['main']['verbose'] >= $level) {
+			echo date('Y-m-d H:i:s').' '.$l.' '.$s."\n";
+		}
+
+		# is loglevel on and level is enough - the try to append to log
+		if (
+			$config['main']['loglevel'] &&
+			$config['main']['loglevel'] >= $level &&
+			$link
+		) {
+
+			# log the error
+			$iu = dbpia($link, array(
+				'message' => $s,
+				'type' => $level,
+				'created' => date('Y-m-d H:i:s')
+			));
+			$sql = 'INSERT INTO invoicenagger_log ('.implode(',', array_keys($iu)).') VALUES('.implode(',', $iu).')';
+			$r = db_query($link, $sql);
+			if ($r === false) {
+				echo db_error($link);
+				die(1);
+			}
+
+		}
+
+		return true;
+	}
+
 ?>
--- index.php
+++ index.php
@@ -1,106 +1,184 @@
 <?php
 	# changelog
-	# 2017-02-14 17:31:52
-
-	# to do a logmessage
-	function logmessage($link, $type, $message) {
-		# log the error
-		$iu = dbpia($link, array(
-			'message' => $message,
-			'type' => $type,
-			'created' => date('Y-m-d H:i:s')
-		));
-		$sql = 'INSERT INTO invoicenagger_log ('.implode(',', array_keys($iu)).') VALUES('.implode(',', $iu).')';
-		$r = db_query($link, $sql);
-		if ($r === false) {
-			echo db_error($link);
-			die(1);
-		}
-	}
-
-	# to disable a debtor
-	function set_debtor_status($link, $id, $status) {
-		# disable this debtor
-		$sql = '
-			UPDATE
-				invoicenagger_debtors
-			SET
-					status="'.dbres($link, $status).'",
-					updated="'.dbres($link, date('Y-m-d H:i:s')).'"
-			WHERE id="'.dbres($link, $id).'"';
-		$r = db_query($link, $sql);
-		if ($r === false) {
-			echo db_error($link);
-			die(1);
-		}
-	}
-
-	# to disable all debtors with a certain template
-	function disable_debtors_with_template($link, $template) {
-		# disable this debtor
-		$sql = '
-			UPDATE
-				invoicenagger_debtors
-			SET
-				status="'.dbres($link,DEBTOR_STATUS_ERROR).'",
-				updated="'.dbres($link, date('Y-m-d H:i:s')).'"
-			WHERE
-				template="'.dbres($link, $template).'"
-				';
-		$r = db_query($link, $sql);
-		if ($r === false) {
-			echo db_error($link);
-			die(1);
-		}
-		return true;
-	}
+	# 2017-02-14 17:31:52 - initial version
+	# 2017-02-17 00:54:23 - updating
 
 	require_once('include/functions.php');
 
-	$opts = getopt('a:', array('action:'));
+	$opts = getopt('a:dv:', array('action:', 'dryrun', 'verbose:'));
 
 	$action = false;
+
+	# default config
+	$config = array(
+		'main' => array(
+			'verbose' => VERBOSE_ERROR,
+			'loglevel' => VERBOSE_INFO,
+			'dryrun' => false
+		)
+	);
+
+	$config_opt = $config;
+
+	# walk parameters
 	foreach ($opts as $k => $v) {
+		# find out what parameter that is used
 		switch ($k) {
 			case 'a':
 			case 'action':
 				$action = $v;
 				break;
+			case 'd': # only dry-run (do not run command)
+			case 'dryrun':
+				$config_opt['main']['dryrun'] = true;
+				cl($link, VERBOSE_DEBUG, 'Dryrun mode activated');
+				break;
+			case 'h':
+			case 'help':
+?>
+Invoice reminder application
+
+-a=<action>, --action=<action>
+	To run an action
+	<action>
+		remind
+			To check for and send reminders
+		errorreset
+			To reset all errors on all debtors with status error by setting
+			them back to active. (Does not change inactivated debtors)
+		remindreset
+			To reset all last reminded dates on all active debtors.
+
+-d, --dryrun
+	Do not send any mails, for testing purposes.
+
+-v[v,vv,vvv], --verbose[v,vv,vvv]
+	To set verbosity
+<?php
+				die(0);
+			case 'v': # be verbose
+			case 'verbose':
+				# determine and set level of verbosity
+				switch ($v) {
+					default: # error
+						$config_opt['main']['verbose'] = VERBOSE_ERROR;
+						break;
+					case 'v': # error, info
+						$config_opt['main']['verbose'] = VERBOSE_INFO;
+						break;
+					case 'vv': # error, info, debug
+						$config_opt['main']['verbose'] = VERBOSE_DEBUG;
+						break;
+					case 'vvv': # error, info, debug, debug deep
+						$config_opt['main']['verbose'] = VERBOSE_DEBUG_DEEP;
+						break;
+				}
+
+				break;
 		}
+		# make sure the config from parameters override all
+		$config = array_merge_recursive_distinct($config, $config_opt);
 	}
 
 	# walk actions
 	switch ($action) {
-		case 'remind':
-			# get all active debtors
-			$sql = '
-				SELECT
-					*
-				FROM
-					invoicenagger_debtors
-				WHERE
-					status='.DEBTOR_STATUS_ACTIVE.'
-					AND
-					last_reminder <= timestampadd(day, -reminder_days, now())
-				';
-			$debtors = 	db_query($link, $sql);
 
-			# walk debtors
-			foreach ($debtors as $debtor) {
+		case 'errorreset': # to reset all errors
+
+			# log it
+			cl(
+				$link,
+				VERBOSE_INFO,
+				'Resetting all errored debtors to active.'
+			);
+
+			$sql = '
+				UPDATE
+					invoicenagger_debtors
+				SET
+					status="'.dbres($link, DEBTOR_STATUS_ACTIVE).'"
+				WHERE
+					status="'.dbres($link, DEBTOR_STATUS_ERROR).'"';
+			cl($link, VERBOSE_DEBUG_DEEP, 'SQL: '.$sql);
+			$r = db_query($link, $sql);
+			if ($r === false) {
+				cl($link, VERBOSE_ERROR, db_error($link).' SQL: '.$sql);
+				die(1);
+			}
+			die(0);
+
+		case 'remind':
+
+			# log it
+			cl(
+				$link,
+				VERBOSE_DEBUG,
+				'Searching for debtors to remind'
+			);
+
+			# loop while there are debtors
+			do {
+				# get all active debtors with active status and not reminded yet
+				$sql = '
+					SELECT
+						*
+					FROM
+						invoicenagger_debtors
+					WHERE
+						status='.dbres($link, DEBTOR_STATUS_ACTIVE).'
+						AND
+						last_reminder <= timestampadd(day, -reminder_days,now())
+						AND (
+							day_of_month = 0
+							OR
+							DAYOFMONTH(NOW()) >= day_of_month
+						)
+					LIMIT 1;
+					';
+				cl($link, VERBOSE_DEBUG_DEEP, 'SQL: '.$sql);
+				$debtor = db_query($link, $sql);
+				if ($debtor === false) {
+					cl($link, VERBOSE_ERROR, db_error($link).' SQL: '.$sql);
+					die(1);
+				}
+
+				# no debtor found?
+				if (!count($debtor) || !isset($debtor[0])) {
+
+					# log it
+					cl($link, VERBOSE_DEBUG, 'No more debtors found');
+
+					# get out
+					break;
+				}
+
+				# simplify debtor
+				$debtor = reset($debtor);
 
 				# get the template
 				$templatefile = TEMPLATE_DIR.$debtor['template'];
 
+				# log it
+				cl($link, VERBOSE_DEBUG, 'Using template: '.$templatefile);
+
+
 				# no template file, fatal error
 				if (!$templatefile) {
+
 					# disable all debtors with this template
 					disable_debtors_with_template($link, $debtor['template']);
-					# set_debtor_status($link, $debtor['id'], DEBTOR_STATUS_ERROR);
 
 					# log it
-					logmessage($link, LOG_TYPE_ERROR, TEMPLATE_DIR.$debtor['template'].' does not exist');
+					cl(
+						$link,
+						VERBOSE_ERROR,
+						TEMPLATE_DIR.
+							$debtor['template'].
+							' does not exist'
+					);
 
-					die(1);
+					# take next debtor
+					continue;
 				}
 
 				# get template file
@@ -112,13 +190,17 @@
 					# disable all debtors with this template
 					disable_debtors_with_template($link, $debtor['template']);
 
-					# disable this debtor
-					# set_debtor_status($link, $debtor['id'], DEBTOR_STATUS_ERROR);
-
 					# log it
-					logmessage($link, LOG_TYPE_ERROR, TEMPLATE_DIR.$debtor['template'].' is not readable');
+					cl(
+						$link,
+						VERBOSE_ERROR,
+						TEMPLATE_DIR.
+							$debtor['template'].
+							' is not readable'
+					);
 
-					die(1);
+					# take next debtor
+					continue;
 				}
 
 				$template = trim($template);
@@ -129,14 +211,17 @@
 					# disable all debtors with this template
 					disable_debtors_with_template($link, $debtor['template']);
 
-					# disable this debtor
-					# set_debtor_status($link, $debtor['id'], DEBTOR_STATUS_ERROR);
-
 					# log it
-					logmessage($link, LOG_TYPE_ERROR, TEMPLATE_DIR.$debtor['template'].' is empty');
+					cl(
+						$link,
+						VERBOSE_ERROR,
+						TEMPLATE_DIR.
+							$debtor['template'].
+							' is empty'
+					);
 
-					die(1);
-
+					# take next debtor
+					continue;
 				}
 
 				# extract subject
@@ -145,21 +230,18 @@
 					# disable all debtors with this template
 					disable_debtors_with_template($link, $debtor['template']);
 
-					# disable this debtor
-					# set_debtor_status($link, $debtor['id'], DEBTOR_STATUS_ERROR);
-
 					# log it
-					logmessage($link, LOG_TYPE_ERROR, TEMPLATE_DIR.$debtor['template'].' is missing subject/body divider ---');
+					cl(
+						$link,
+						VERBOSE_ERROR,
+						TEMPLATE_DIR.$debtor['template'].
+							' is missing subject/body divider ---'
+					);
 
-					die(1);
-
+					# take next debtor
+					continue;
 				}
 
-				# find subject
-				$subject = trim(substr($template, 0, strpos($template, '---')));
-
-				# find body
-				$body = trim(substr($template, strpos($template, '---') + 3));
 
 				# start date and end date for interest calculation
 				$date1 = new DateTime($debtor['duedate']);
@@ -168,47 +250,102 @@
 				# no need to remove one day here, as PHP does not add one
 				$days_elapsed = $date2->diff($date1)->format("%a");
 
+				# calculate interest for one day
 				$perday = (($debtor['amount']) * $debtor['percentage']) / 365;
 
-
+				# calculate interest for all days that has elapsed
 				$interest = $perday * $days_elapsed;
 
-				$total = $debtor['amount'] + $interest + $debtor['collectioncost'];
+				# summarize all costs
+				$total = $debtor['amount'];
+				$total += $interest;
+				$total += $debtor['collectioncost'];
+
+				$placeholders_must_exist = array(
+					'$AMOUNT$',
+					'$DUEDATE$',
+					'$INVOICEDATE$',
+					'$INVOICENUMBER$',
+					'$PERCENTAGE$',
+					'$TOTAL$'
+				);
 
 				$placeholders = array(
-					'$AMOUNT$' => number_format($debtor['amount'], 2),
-					'$COLLECTIONCOST$' => number_format($debtor['collectioncost'], 2),
+					'$ADDRESS$' => $debtor['address'],
+					'$AMOUNT$' => number_format($debtor['amount'], 2, ',',','),
+					'$CITY$' => $debtor['city'],
+					'$COLLECTIONCOST$' => number_format(
+						$debtor['collectioncost'], 2
+					),
 					'$DUEDATE$' => $debtor['duedate'],
 					'$INTERESTDATE$' => date('Y-m-d'),
-					'$INTEREST$' => number_format($interest, 2),
+					'$INTEREST$' => number_format($interest, 2, ',',','),
+					'$INTERESTRAISE$' => number_format($perday, 2, ',',','),
 					'$INVOICEDATE$' => $debtor['invoicedate'],
+					'$NAME$' => $debtor['name'],
 					'$INVOICENUMBER$' => $debtor['invoicenumber'],
-					'$PERCENTAGE$' => number_format($debtor['percentage'] * 100, 2),
-					'$REMINDERCOST$' => number_format($debtor['remindercost'], 2),
-					'$TOTAL$' => number_format($total, 2)
+					'$ORGNO$' => $debtor['orgno'],
+					'$PERCENTAGE$' => number_format(
+						$debtor['percentage'] * 100, 2
+					),
+					'$REMINDERCOST$' => number_format(
+						$debtor['remindercost'], 2
+					),
+					'$TOTAL$' => number_format($total, 2, ',',','),
+					'$ZIPCODE$' => $debtor['zipcode'],
+					'$EMAIL$' => $debtor['email']
 				);
+
+				# log it
+				cl($link, VERBOSE_DEBUG, 'Filling template placeholders');
 
 				# make sure all locations exist
 				foreach ($placeholders as $placeholderk => $placeholderv) {
 
 					# does this placeholder not exist in the template body?
-					if (strpos($body, $placeholderk) === false) {
-						# disable this debtor
-						set_debtor_status($link, $debtor['id'], DEBTOR_STATUS_ERROR);
+					if (strpos($template, $placeholderk) === false) {
 
-						# log it
-						logmessage($link, LOG_TYPE_ERROR, TEMPLATE_DIR.$debtor['template'].' is missing placeholder '.$placeholderk);
+						# is it a required value?
+						if (in_array($placeholderk, $placeholders_must_exist)) {
+							# disable this debtor
+							set_debtor_status(
+								$link,
+								$debtor['id'],
+								DEBTOR_STATUS_ERROR
+							);
 
-						die(1);
+							# log it
+							cl(
+								$link,
+								VERBOSE_ERROR,
+								TEMPLATE_DIR.$debtor['template'].
+									' is missing placeholder '.
+									$placeholderk
+							);
+							# take next debtor
+							continue 2;
 
+						}
+
+						# take next placeholder
+						continue 1;
 					}
 
 					# fill the placeholder
-					$body = str_replace($placeholderk, $placeholderv, $body);
+					$template = str_replace($placeholderk, $placeholderv, $template);
 				}
 
-				# mail is ready, send it
+				# find subject
+				$subject = trim(substr($template, 0, strpos($template, '---')));
 
+				# find body
+				$body = trim(substr($template, strpos($template, '---') + 3));
+
+
+				# log it
+				cl($link, VERBOSE_INFO, 'Sending mail to: '.$debtor['email']);
+
+				# mail is ready, send it
 				#echo 'SUBJECT: '.$subject."\n\n";
 				#echo 'BODY: '."\n".$body."\n\n";
 
@@ -218,46 +355,114 @@
 
 				# additional headers
 				# $headers[] = 'To: Mary <mary@example.com>';
-				$headers[] = 'From: Your Name <your@email.com>';
+				$headers[] = 'From: '.FROM;
+				$headers[] = 'Reply-To: '.REPLY_TO;
+
+				# is there a bcc address supplied
 				if (strlen($debtor['email_bcc'])) {
+					# then add the bcc header
 					$headers[] = 'Bcc: '.$debtor['email_bcc'];
 				}
 
-				# try to send the mail
-				$mail_sent = mail(
-					$debtor['email'],
-					$subject,
-					$body,
-					implode("\r\n", $headers)
+				cl(
+					$link,
+					VERBOSE_DEBUG,
+					"\n".
+						str_repeat('-', 80)."\n".
+						implode(
+							str_repeat('-', 80)."\n",
+							array(
+								'To: '.$debtor['email']."\n".implode("\n", $headers)."\n",
+								$subject."\n",
+								$body
+							)
+						)."\n".
+						str_repeat('-', 80)."\n"
 				);
 
+
+				# try to send the mail
+				if (!$config_opt['main']['dryrun']) {
+					$mail_sent = mail(
+						$debtor['email'],
+						$subject,
+						$body,
+						implode("\r\n", $headers)
+					);
+				} else {
+					$mail_sent = true;
+				}
+
+				# did mail fail?
 				if ($mail_sent === false) {
 					# disable this debtor
-					set_debtor_status($link, $debtor['id'], DEBTOR_STATUS_ERROR);
+					set_debtor_status(
+						$link,
+						$debtor['id'],
+						DEBTOR_STATUS_ERROR
+					);
 
 					# log it
-					logmessage($link, LOG_TYPE_ERROR, 'Failed sending mail to '.$debtor['email'].' (bcc: '.$debtor['email_bcc'].')');
+					cl(
+						$link,
+						VERBOSE_ERROR,
+						'Failed sending mail to '.
+							$debtor['email'].' (bcc: '.
+							$debtor['email_bcc'].')'
+					);
+
+					# take next debtor
 					continue;
 				}
 
-				# update last reminder
+				# update last reminder on this debtor
 				$sql = '
 					UPDATE
 						invoicenagger_debtors
 					SET
+						updated="'.dbres($link, date('Y-m-d H:i:s')).'",
 						last_reminder="'.dbres($link, date('Y-m-d H:i:s')).'",
 						mails_sent=mails_sent+1
 					WHERE
 						id="'.dbres($link, $debtor['id']).'"
 					';
+				cl($link, VERBOSE_DEBUG_DEEP, 'SQL: '.$sql);
 				$r = db_query($link, $sql);
 				if ($r === false) {
-					echo db_error($link);
+					cl($link, VERBOSE_ERROR, db_error($link).' SQL: '.$sql);
 					die(1);
 				}
 
-				logmessage($link, LOG_TYPE_MAIL_SENT, $debtor['email']);
+				# log that mail has been sent
+				cl($link, VERBOSE_INFO, $debtor['email']);
 
-			} # walk debtors
+			} while (true);
+
+			die(0);
+
+		case 'remindreset': # to reset last reminded dates on active debtors
+
+			# log it
+			cl(
+				$link,
+				VERBOSE_INFO,
+				'Resetting all active debtor reminder dates.'
+			);
+
+			$sql = '
+				UPDATE
+					invoicenagger_debtors
+				SET
+					last_reminder="'.dbres($link, '1970-01-01 00:00:00').'"
+				WHERE
+					status="'.dbres($link, DEBTOR_STATUS_ACTIVE).'"';
+			cl($link, VERBOSE_DEBUG_DEEP, 'SQL: '.$sql);
+			$r = db_query($link, $sql);
+			if ($r === false) {
+				cl($link, VERBOSE_ERROR, db_error($link).' SQL: '.$sql);
+				die(1);
+			}
+			die(0);
+
 	}
 ?>
